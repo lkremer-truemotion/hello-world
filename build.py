73a5a2a27f0f5e0c09c299fac7a6476c09d76d74
laszlo.kremer <laszlo.kremer@gotruemotion.com>
2022-09-14 17:42:13 +0200

    show exit code

--- build.py
+++ build.py
@@ -1,2 +1,5 @@
-def commit():
-    local("git add -p && git commit")
+import subprocess
+
+result = subprocess.Popen("git add -p && git commit")
+text = result.communicate()[0]
+return_code = result.returncode
