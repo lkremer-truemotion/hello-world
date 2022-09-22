fe18f33787f971f32c5bfd700c97be9bc2263c6a
laszlo.kremer <laszlo.kremer@gotruemotion.com>
2022-09-22 16:18:18 +0200

    trying to get return code 5

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ import subprocess
 
 #process = subprocess.run(["git","add","-p","&&","git","commit"])
 
-runcommand( "git add -p && git commit")
+cmd("git add -p && git commit")
 
 def runcommand (cmd):
     proc = subprocess.Popen(cmd,
@@ -12,3 +12,33 @@ def runcommand (cmd):
                             universal_newlines=True)
     std_out, std_err = proc.communicate()
     return proc.returncode, std_out, std_err
+
+def main():
+    print("==================================================");
+    print('Running "ls -lh"...');
+    print("==================================================");
+    code, out, err = runcommand("ls -lh");
+    print("Return code: {}".format(code));
+    print("--------------------------------------------------");
+    print("stdout:");
+    print(out);
+    print("--------------------------------------------------");
+    print("stderr:");
+    print(err);
+    print("--------------------------------------------------");
+
+    print("==================================================");
+    print('Running "ls -lj"...');
+    print("==================================================");
+    code, out, err = runcommand("ls -lj");
+    print("Return code: {}".format(code));
+    print("--------------------------------------------------");
+    print("stdout:");
+    print(out);
+    print("--------------------------------------------------");
+    print("stderr:");
+    print(err);
+    print("--------------------------------------------------");
+    
+if __name__ == '__main__':
+    main()
