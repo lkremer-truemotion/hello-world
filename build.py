25212b082b1ecf151777084ec5a9d445ff026289
laszlo.kremer <laszlo.kremer@gotruemotion.com>
2022-09-22 15:29:53 +0200

    change Popen to run

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 import subprocess
 
-result = subprocess.Popen("git add -p && git commit")
+result = subprocess.run("git add -p && git commit")
 text = result.communicate()[0]
 return_code = result.returncode
