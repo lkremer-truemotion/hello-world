4274f8477dad8099810b8e7250dbac6d56243e01
laszlo.kremer <laszlo.kremer@gotruemotion.com>
2022-09-22 16:13:37 +0200

    trying to get return code 3

--- build.py
+++ build.py
@@ -1,6 +1,14 @@
 import subprocess
 
-process = subprocess.run(["git","add","-p","&&","git","commit"])
-stdout, stderr = process.communicate()
-exit_code = process.wait()
-print(stdout, stderr, exit_code)
+#process = subprocess.run(["git","add","-p","&&","git","commit"])
+
+cmd = "git add -p && git commit"
+
+def runcommand (cmd):
+    proc = subprocess.Popen(cmd,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            shell=True,
+                            universal_newlines=True)
+    std_out, std_err = proc.communicate()
+    return proc.returncode, std_out, std_err
