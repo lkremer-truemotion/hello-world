af6d11725f69d4483deaf7303582a59069ad0193
laszlo.kremer <laszlo.kremer@gotruemotion.com>
2022-09-22 16:07:01 +0200

    trying to get return code 2

--- build.py
+++ build.py
@@ -1,4 +1,6 @@
 import subprocess
 
-result = subprocess.run(["git","add","-p","&&","git","commit"])
-return_code = result.returncode
+process = subprocess.run(["git","add","-p","&&","git","commit"])
+stdout, stderr = process.communicate()
+exit_code = process.wait()
+print(stdout, stderr, exit_code)
